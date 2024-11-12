9405e745b685893e83cf23cf37756971e62ba399
DenisAnufriev <wenomavel89@gmail.com>
2024-11-13 00:34:44 +0300

    update settings.py add csu

--- config/settings.py
+++ config/settings.py
@@ -75,6 +75,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'],
+}
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
