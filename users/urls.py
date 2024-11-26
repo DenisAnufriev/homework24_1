c1cadc3b32670d4730783ecdd4c23da7d7ce2b36
DenisAnufriev <wenomavel89@gmail.com>
2024-11-27 02:41:56 +0300

    update urls

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,15 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import (PaymentCreateAPIView, UserCreateAPIView,
-                         UserDestroyAPIView, UserListAPIView,
-                         UserRetrieveAPIView, UserUpdateAPIView, SubscriptionAPIView)
+from users.views import (
+    PaymentCreateAPIView,
+    UserCreateAPIView,
+    UserDestroyAPIView,
+    UserListAPIView,
+    UserRetrieveAPIView,
+    UserUpdateAPIView,
+    SubscriptionAPIView,
+)
 
 app_name = UsersConfig.name
 
@@ -16,7 +22,7 @@ urlpatterns = [
         UserCreateAPIView.as_view(permission_classes=[AllowAny]),
         name="register",
     ),
-    path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user_detail"),
+    path("users/<int:pk>/", UserRetrieveAPIView.as_view(), name="user_detail"),
     path("edit/<int:pk>/", UserUpdateAPIView.as_view(), name="user_edit"),
     path("delete/<int:pk>/", UserDestroyAPIView.as_view(), name="user_delete"),
     path("payment/", PaymentCreateAPIView.as_view(), name="payments"),
