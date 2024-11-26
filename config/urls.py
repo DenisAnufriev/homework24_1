c9f9ca48b9d41b440dfd53c6e482173eb6b2571f
DenisAnufriev <wenomavel89@gmail.com>
2024-11-27 00:06:44 +0300

    add path schema/swagger/redoc

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+                                   SpectacularSwaggerView)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("learning/", include("lms.urls", namespace="learning")),
     path("users/", include("users.urls", namespace="users")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
+    ),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import (PaymentListAPIView, UserCreateAPIView,
+from users.views import (PaymentCreateAPIView, UserCreateAPIView,
                          UserDestroyAPIView, UserListAPIView,
                          UserRetrieveAPIView, UserUpdateAPIView, SubscriptionAPIView)
 
@@ -19,7 +19,7 @@ urlpatterns = [
     path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user_detail"),
     path("edit/<int:pk>/", UserUpdateAPIView.as_view(), name="user_edit"),
     path("delete/<int:pk>/", UserDestroyAPIView.as_view(), name="user_delete"),
-    path("payment/", PaymentListAPIView.as_view(), name="user_payments"),
+    path("payment/", PaymentCreateAPIView.as_view(), name="payments"),
     path(
         "login/",
         TokenObtainPairView.as_view(permission_classes=[AllowAny]),
