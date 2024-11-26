05c2263c83cd697619650d7009e68ec5249aac70
DenisAnufriev <wenomavel89@gmail.com>
2024-11-27 02:42:43 +0300

    update schema_field

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.fields import IntegerField, BooleanField
+from drf_spectacular.utils import extend_schema_field
 
 from lms.models import Course, Lesson
 from lms.validators import validate_youtube_only
@@ -10,7 +12,13 @@ class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
-        fields = ("id", "title", "description", "link_to_video", "course",)
+        fields = (
+            "id",
+            "title",
+            "description",
+            "link_to_video",
+            "course",
+        )
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -20,11 +28,20 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ("id", "title", "description", "lessons_count", "lessons", "is_subscribed")
+        fields = (
+            "id",
+            "title",
+            "description",
+            "lessons_count",
+            "lessons",
+            "is_subscribed",
+        )
 
+    @extend_schema_field(IntegerField)
     def get_lessons_count(self, obj):
         return obj.lessons.count()
 
+    @extend_schema_field(BooleanField)
     def get_is_subscribed(self, obj):
         user = self.context["request"].user
         return Subscription.objects.filter(user=user, course=obj).exists()
