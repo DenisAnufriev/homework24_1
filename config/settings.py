f21dbc150825aaa43f11892e82a7c94eb5e8d601
DenisAnufriev <wenomavel89@gmail.com>
2024-11-27 02:42:14 +0300

    update

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'],
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
